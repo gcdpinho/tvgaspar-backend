2b6c7436ea9223d444472c00e1b229b412005d60
Gustavo Pinho <gustavo.pinho@gavb.com.br>
2018-01-26 17:31:49 -0200

    first commit

--- dao/usuario.js
+++ dao/usuario.js
@@ -20,7 +20,7 @@ const getAllUsuarios = function (req, res, data) {
 }
 
 const findByNome = function (req, res) {
-    console.log(req.body.nome);
+    console.log(req.body);
     service(query.selectByNome, req, res, req.body.nome, function (results) {
         if (!results)
             res.json({
