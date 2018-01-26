2d48c7ecc18613a2f54616570707d106ceb3e18c
Gustavo Pinho <gcdpinho@gmail.com>
2018-01-26 21:49:13 -0200

    ajust adm

--- dao/usuario.js
+++ dao/usuario.js
@@ -6,14 +6,19 @@ const cipher = crypto.createCipher(config.criptografia.alg, config.criptografia.
 
 const query = {
     insert: "INSERT INTO usuario (nome, senha, adm) VALUES (?, ?, ?)",
+    updateToken: "UPDATE usuario SET token = ? WHERE nome = ?",
     selectAll: "SELECT * FROM usuario",
     selectByNome: "SELECT * FROM usuario WHERE nome = ?"
 }
 
-const createUsuario = function (req, res, data) {
-    cipher.update(data.senha);
-    data.senha = cipher.final(config.criptografia.tipo);
-    service(query.insert, req, res, [data.nome, data.senha, data.adm], "");
+const createUsuario = function (req, res) {
+    cipher.update(req.body.senha);
+    req.body.senha = cipher.final(config.criptografia.tipo);
+    service(query.insert, req, res, [req.body.nome, req.body.senha, req.body.adm], "");
+}
+
+const updateToken = function (req, res, data) {
+    service(query.updateToken, req, res, [data.token, data.nome], "");
 }
 
 const getAllUsuarios = function (req, res) {
@@ -47,7 +52,11 @@ const findByNome = function (req, res) {
                 res.json({
                     success: true,
                     message: 'Token criado com sucesso.',
-                    token: token
+                });
+
+                updateToken(req, res, {
+                    token: token,
+                    nome: req.body.nome
                 });
             }
         }
