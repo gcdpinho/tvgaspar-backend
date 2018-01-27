8252338d3f776097c0a49cc5d4eae38b249d2a4b
Gustavo Pinho <gcdpinho@gmail.com>
2018-01-27 11:55:26 -0200

    ajusts adm

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const config = require('./config.js');
-const jwt = require('jsonwebtoken');
-router.use(function (req, res, next) {
-    var token = req.body.token || req.query.token || req.headers['token'];
-
-    if (token) {
-        jwt.verify(token, config.criptografia.secret, function (error, decoded) {
-            if (error)
-                return res.json({
-                    sucess: false,
-                    message: "Falha ao autenticar o token."
-                });
-            else {
-                req.decoded = decoded;
-                next();
-            }
-        });
-    } else
-        return res.status(403).send({
-            sucess: false,
-            message: "Requisição sem token."
-        });
-});
 const bodyParser = require('body-parser');
 const usuario = require('./routes/usuario.js')(router);
 
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,13 +1,33 @@
 const usuario = require('./../dao/usuario.js');
+const config = require('./../config.js');
+const jwt = require('jsonwebtoken');
 
 module.exports = function (router) {
 
+    router.use(function (req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['token'];
+
+        if (token) {
+            jwt.verify(token, config.criptografia.secret, function (error, decoded) {
+                if (error)
+                    return res.json({
+                        sucess: false,
+                        message: "Falha ao autenticar o token."
+                    });
+                else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else
+            return res.status(403).send({
+                sucess: false,
+                message: "Requisição sem token."
+            });
+    });
+    
     router.get('/createUsuario', (req, res) => {
-        usuario.createUsuario(req, res, {
-            nome: "teste",
-            senha: "teste12345",
-            adm: 0
-        });
+        usuario.createUsuario(req, res);
     });
 
     router.get('/getAllUsuarios', (req, res) => {
