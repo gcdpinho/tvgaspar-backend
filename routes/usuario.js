f1fced35057f38943f5751b38491c49bf9e3de05
Gustavo Pinho <gustavo.pinho@gavb.com.br>
2018-01-29 10:41:07 -0200

    ajusts adm

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,6 +2,34 @@ const usuario = require('./../dao/usuario.js');
 const config = require('./../config.js');
 const jwt = require('jsonwebtoken');
 
+const verifyToken = function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['token'];
+
+    if (token) {
+        jwt.verify(token, config.criptografia.secret, function (error, decoded) {
+            if (error) {
+                if (error.name == "TokenExpiredError") {
+                    req.body.nome = req.query.nome;
+                    usuario.createToken(res, req);
+                }
+                return res.json({
+                    sucess: false,
+                    message: "Falha ao autenticar o token."
+                });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else
+        return res.status(403).send({
+            sucess: false,
+            message: "Requisição sem token."
+        });
+}
+
+
+
 module.exports = function (router) {
 
     router.post('/login', (req, res) => {
@@ -19,29 +47,5 @@ module.exports = function (router) {
     });
 
 
-    const verifyToken = function (req, res, next) {
-        var token = req.body.token || req.query.token || req.headers['token'];
-
-        if (token) {
-            jwt.verify(token, config.criptografia.secret, function (error, decoded) {
-                if (error) {
-                    if (error.name == "TokenExpiredError") {
-                        req.body.nome = req.query.nome;
-                        usuario.createToken(res, req);
-                    }
-                    return res.json({
-                        sucess: false,
-                        message: "Falha ao autenticar o token."
-                    });
-                } else {
-                    req.decoded = decoded;
-                    next();
-                }
-            });
-        } else
-            return res.status(403).send({
-                sucess: false,
-                message: "Requisição sem token."
-            });
-    }
+    
 }
