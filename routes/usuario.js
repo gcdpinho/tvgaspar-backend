cd101a421c9151b711c1ae343ba835a6696ca127
Gustavo Pinho <gustavo.pinho@gavb.com.br>
2018-01-29 14:57:30 -0200

    ajust adm

--- routes/usuario.js
+++ routes/usuario.js
@@ -26,6 +26,13 @@ const verifyToken = function (req, res, next) {
 
 module.exports = function (router) {
 
+    router.get('/', function(req, res) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+        res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    });
+
     router.post('/login', (req, res) => {
         usuario.findByNome(req, res);
     });
