a26233e3a6508654a515fd1338be04e52932e98b
Gustavo Pinho <gustavo.pinho@gavb.com.br>
2018-02-01 17:31:17 -0200

    ajustes serviços

--- functions.js
+++ functions.js
@@ -29,8 +29,8 @@ const service = function (query, req, res, data, callback, model, multiples) {
     if (multiples) {
         console.log(data);
         data.forEach(function (element, index) {
-            console.log(element);
-            connection.query(query, element, function (error, results, fields) {
+            console.log(element.values());
+            connection.query(query, element.values(), function (error, results, fields) {
                 if (error)
                     res.json(error);
                 else {
