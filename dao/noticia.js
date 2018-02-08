562f6d334d66a7a6c96972b56934f5d83266621a
Gustavo Pinho <gcdpinho@gmail.com>
2018-02-07 23:19:54 -0200

    fix noticias model + dao

--- dao/noticia.js
+++ dao/noticia.js
@@ -3,7 +3,7 @@ const modelNoticia = require('./../model/noticia.js');
 
 /* Queries */
 const query = {
-    insert: "INSERT INTO noticia (manchete, subManchete, texto, autor, dtCadastro, flgAtivo, idUsuario) VALUES (?, ?, ?, ?, STR_TO_DATE(?, '%d/%m/%Y %H:%i'), ?, ?)",
+    insert: "INSERT INTO noticia (manchete, subManchete, texto, autor, dtCadastro, flgAtivo, idUsuario) VALUES (?, ?, ?, ?, STR_TO_DATE('?', '%d/%m/%Y %H:%i'), ?, ?)",
     update: "UPDATE noticia SET manchete = ?, subManchete = ?, texto = ?, autor = ?, dtCadastro = STR_TO_DATE(?, '%d/%m/%Y %H:%i'), flgAtivo = ?, idUsuario = ? WHERE id = ?",
     selectAll: "SELECT * FROM noticia"
 }
