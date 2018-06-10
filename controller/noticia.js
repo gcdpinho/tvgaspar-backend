3536e092314bcbce2fb7eecd39415578cccb4436
gcdpinho <gcdpinho@gmail.com>
2018-06-10 17:45:07 -0300

    noticia+resume

--- controller/noticia.js
+++ controller/noticia.js
@@ -3,33 +3,46 @@ const modelNoticia = require('./../model/noticia.js');
 const query = require('./../dao/query.js');
 
 /* Services */
-const createNoticia = function (req, res){
-    functions.service(query.noticia.insert, req, res, [req.body.manchete, req.body.subManchete, req.body.texto, req.body.autor, req.body.dtCadastro, req.body.flgAtivo, req.body.aprovacao, req.body.idUsuario], "", modelNoticia, false);
+const createNoticia = function (req, res) {
+    functions.service(query.noticia.insert, req, res, [req.body.manchete, req.body.subManchete, req.body.resumo, req.body.texto, req.body.autor, req.body.dtCadastro, req.body.flgAtivo, req.body.aprovacao, req.body.idUsuario], "", modelNoticia, false);
 }
 
-const getAllNoticias = function (req, res){
+const getAllNoticias = function (req, res) {
     functions.service(query.noticia.selectAll, req, res, "", "default", modelNoticia, false);
 }
 
-const updateNoticia = function (req, res){
-    functions.service(query.noticia.update, req, res, [req.body.manchete, req.body.subManchete, req.body.texto, req.body.autor, req.body.dtCadastro, req.body.id], "", modelNoticia, false);
+const updateNoticia = function (req, res) {
+    functions.service(query.noticia.update, req, res, [req.body.manchete, req.body.subManchete, req.body.resumo, req.body.texto, req.body.autor, req.body.dtCadastro, req.body.id], "", modelNoticia, false);
 }
 
-const updateAprovacao = function(req, res){
+const updateAprovacao = function (req, res) {
     functions.service(query.noticia.updateAprovacao, req, res, [req.body.aprovacao, req.body.id], "", modelNoticia, false);
 }
 
-const deleteById = function(req, res){
+const deleteById = function (req, res) {
     functions.service(query.noticia.deleteById, req, res, [req.body.id], "", modelNoticia, false);
 }
 
-const getAllNoticiasAprovadas = function(req, res){
+const getAllNoticiasAprovadas = function (req, res) {
     functions.service(query.noticia.selectAllNoticiaAprovada, req, res, [], "", modelNoticia, false);
 }
 
-const getNoticiasAprovadasByTag = function(req, res){
+const getNoticiasAprovadasByTag = function (req, res) {
     functions.service(query.noticia.selectNoticiaAprovadaByTag, req, res, [req.body.tituloTag], "", modelNoticia, false);
 }
+
+const getNoticiasAprovadasByCategoria = function (req, res) {
+    functions.service(query.noticia.selectNoticiaAprovadaByCategoria, req, res, [req.body.tituloCategoria], "", modelNoticia, false);
+}
+
+const getNoticiasAprovadasByTagByCategoria = function (req, res) {
+    functions.service(query.noticia.selectNoticiaAprovadaByTagByCategoria, req, res, [req.body.tituloCategoria, req.body.tituloTag], "", modelNoticia, false);
+}
+
+const getNoticiasAprovadasByIdNoticia = function (req, res) {
+    functions.service(query.noticia.selectNoticiaAprovadaByIdNoticia, req, res, [req.body.idNoticia], "", modelNoticia, false);
+}
+
 
 /* end-Services */
 
@@ -40,5 +53,8 @@ module.exports = {
     updateAprovacao,
     deleteById,
     getAllNoticiasAprovadas,
-    getNoticiasAprovadasByTag
+    getNoticiasAprovadasByTag,
+    getNoticiasAprovadasByCategoria,
+    getNoticiasAprovadasByTagByCategoria,
+    getNoticiasAprovadasByIdNoticia
 };
--- routes/site.js
+++ routes/site.js
@@ -9,4 +9,16 @@ module.exports = function (router) {
     router.post('/getNoticiasAprovadasByTag', (req, res) => {
         noticia.getNoticiasAprovadasByTag(req, res);
     });
+
+    router.post('/getNoticiasAprovadasByCategoria', (req, res) => {
+        noticia.getNoticiasAprovadasByCategoria(req, res);
+    });
+
+    router.post('/getNoticiasAprovadasByTagByCategoria', (req, res) => {
+        noticia.getNoticiasAprovadasByTagByCategoria(req, res);
+    });
+
+    router.post('/getNoticiasAprovadasByIdNoticia', (req, res) => {
+        noticia.getNoticiasAprovadasByIdNoticia(req, res);
+    });
 }
